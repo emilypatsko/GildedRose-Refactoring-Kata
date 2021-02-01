ed88202bb474ca7dfc239ad5689d1af8edabb917
Emily Patsko <Emily.Patsko@softwire.com>
2021-02-01 17:17:29 +0000

    Abstracted everything to functions and implemented final unit tests

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -40,17 +40,31 @@ export class GildedRose {
             item.sellIn--;
             if (item.sellIn < 0) {
                 item.quality = 0;
-            } else {
-                if (item.quality < 50) {
-                    item.quality++;
-                }
-                if (item.quality < 50 && item.sellIn < 11) {
-                    item.quality++;
-                }
-                if (item.quality < 50 && item.sellIn < 6) {
-                    item.quality++;
-                }
+                return;
+            } 
+            if (item.quality < 50) {
+                item.quality++;
+            }
+            if (item.quality < 50 && item.sellIn < 10) {
+                item.quality++;
+            }
+            if (item.quality < 50 && item.sellIn < 5) {
+                item.quality++;
             }         
+        }
+    }
+
+    handleRegularItems(item) {
+        if (item.name != 'Aged Brie' && 
+            item.name != 'Backstage passes to a TAFKAL80ETC concert' && 
+            item.name != 'Sulfuras, Hand of Ragnaros') {
+            if (item.quality > 0) {
+                item.quality--;
+            }
+            item.sellIn--;
+            if (item.sellIn < 0 && item.quality > 0) {
+                item.quality--;
+            }
         }
     }
 
@@ -59,51 +73,8 @@ export class GildedRose {
             this.handleSulfuras(this.items[i]);
             this.handleAgedBrie(this.items[i]);
             this.handleBackstagePasses(this.items[i]);
-            // if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            //     if (this.items[i].quality > 0) {
-            //         if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            //             this.items[i].quality = this.items[i].quality - 1
-            //         }
-            //     }
-            // } else {
-            //     if (this.items[i].quality < 50) {
-            //         this.items[i].quality = this.items[i].quality + 1
-            //         if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            //             if (this.items[i].sellIn < 11) {
-            //                 if (this.items[i].quality < 50) {
-            //                     this.items[i].quality = this.items[i].quality + 1
-            //                 }
-            //             }
-            //             if (this.items[i].sellIn < 6) {
-            //                 if (this.items[i].quality < 50) {
-            //                     this.items[i].quality = this.items[i].quality + 1
-            //                 }
-            //             }
-            //         }
-            //     }
-            // }
-            // if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            //     this.items[i].sellIn = this.items[i].sellIn - 1;
-            // }
-            // if (this.items[i].sellIn < 0) {
-            //     if (this.items[i].name != 'Aged Brie') {
-            //         if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            //             if (this.items[i].quality > 0) {
-            //                 if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            //                     this.items[i].quality = this.items[i].quality - 1
-            //                 }
-            //             }
-            //         } else {
-            //             this.items[i].quality = this.items[i].quality - this.items[i].quality
-            //         }
-            //     } else {
-            //         if (this.items[i].quality < 50) {
-            //             this.items[i].quality = this.items[i].quality + 1
-            //         }
-            //     }
-            // }
+            this.handleRegularItems(this.items[i]);
         }
-
         return this.items;
     }
 }
